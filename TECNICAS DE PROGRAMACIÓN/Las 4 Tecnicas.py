cbd2ae4c94d8c011495ec9f5a21bb7d985bbf500
Elcy Ramos <ed.ramoss@uea.edu.ec>
2025-06-14 03:42:45 +0200

    semana 2 poo arr

--- TECNICAS DE PROGRAMACIÓN/Las 4 Tecnicas.py
+++ TECNICAS DE PROGRAMACIÓN/Las 4 Tecnicas.py
@@ -1,37 +1,37 @@
-# Tecnicas de la POO
+# Técnicas de la POO
 from abc import ABC, abstractmethod
 
 # Abstracción
 # Clase base
 class Articulo(ABC):
-    def __init__(self, titulo, autor, precio):
-        self._titulo = titulo
-        self._autor = autor
-        self._precio = precio
+    def __init__(self, __titulo, __autor, __precio):
+        self.__titulo = __titulo
+        self.__autor = __autor
+        self.__precio = __precio
 
     @property
     def titulo(self):
-        return self._titulo
+        return self.__titulo
 
     @titulo.setter
     def titulo(self, value):
-        self._titulo = value
+        self.__titulo = value
 
     @property
     def autor(self):
-        return self._autor
+        return self.__autor
 
     @autor.setter
     def autor(self, value):
-        self._autor = value
+        self.__autor = value
 
     @property
     def precio(self):
-        return self._precio
+        return self.__precio
 
     @precio.setter
     def precio(self, value):
-        self._precio = value
+        self.__precio = value
 
     @abstractmethod
     def mostrar_detalles(self):
@@ -40,48 +40,43 @@ class Articulo(ABC):
 # Herencia y Polimorfismo
 # Clase hija 1
 class Libro(Articulo):
-    def __init__(self, titulo, autor, precio, genero):
-        super().__init__(titulo, autor, precio)
-        self.genero = genero
+    def __init__(self, __titulo, __autor, __precio, __genero):
+        super().__init__(__titulo, __autor, __precio)
+        self.__genero = __genero
 
     @property
     def genero(self):
-        return self._genero
+        return self.__genero
 
     @genero.setter
     def genero(self, value):
-        self._genero = value
+        self.__genero = value
 
     def mostrar_detalles(self):
-        print(f"Título: {self._titulo}")
+        print(f"Título: {self.titulo}")
         print(f"Autor: {self.autor}")
         print(f"Precio: {self.precio}")
         print(f"Género: {self.genero}")
 
-# Herencia y Polimorfismo
 # Clase hija 2
 class Periodico(Articulo):
-    def __init__(self, titular, autor, precio_editorial, editorial):
-        super().__init__(titular, autor, precio_editorial)
-        self.editorial = editorial
+    def __init__(self, __titular, __autor, __precio, __editorial):
+        super().__init__(__titular, __autor, __precio)
+        self.__editorial = __editorial
 
     @property
     def editorial(self):
-        return self._editorial
-
-    @editorial.setter
-    def editorial(self, value):
-        self._editorial = value
+        return self.__editorial
 
     def mostrar_detalles(self):
-        print(f"Título: {self._titulo}")
+        print(f"Título: {self.titulo}")
         print(f"Autor: {self.autor}")
         print(f"Precio: {self.precio}")
         print(f"Editorial: {self.editorial}")
 
 # Crear los Objetos
 libro = Libro("A Game of Thrones", "George R. R. Martin", 15.00, "Fantasia")
-periodico = Periodico("El impacto del cambio climático en América Latina", "J. Martínez", 5.00, "El País")
+periodico = Periodico("El impacto del cambio climático en América Latina", "J. Martinez", 5.00, "El País")
 
 # Mostrar los detalles (Polimorfismo)
 libro.mostrar_detalles()
