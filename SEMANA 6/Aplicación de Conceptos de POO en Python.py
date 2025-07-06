9a2cf63123b42e5f44c86eb05aacfe053c1ef915
Elcy Ramos <ed.ramoss@uea.edu.ec>
2025-07-06 16:57:54 +0200

    Semana 6

---
+++ SEMANA 6/Aplicación de Conceptos de POO en Python.py
@@ -0,0 +1,50 @@
+# Clase base
+class Animal:
+    def __init__(self, nombre):
+        self.__nombre = nombre
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+
+    def hablar(self):  # Metodo sobreescrito en las clases derivadas.
+        pass
+
+# Clase derivada
+class Perro(Animal):
+    def hablar(self):
+        return f"{self.get_nombre()} ladra"
+
+    def ladra(self):
+        return "¡Guau!"
+
+# Clase derivada
+class Caballo(Animal):
+    def hablar(self):
+        return f"{self.get_nombre()} relincha"
+
+    def relincha(self):
+        return "¡hiiiii!"
+
+# Uso de polimorfismo
+def animal_habitual_habito(animal):
+    print(animal.hablar())
+
+# Creación de instancias y demostración de funcionalidad
+def main():
+    perro = Perro("Bam")
+    caballo = Caballo("Dante")
+
+    print(perro.get_nombre())
+    print(caballo.get_nombre())
+
+    print(perro.ladra())  # Uso del metodo especifico para Perro.
+    print(caballo.relincha())  # Uso de metodo especifico para Gato.
+
+    animal_habitual_habito(perro)  # Uso de polimorfismo
+    animal_habitual_habito(caballo)  # Uso de polimorfismo
+
+if __name__ == "__main__":
+    main()
