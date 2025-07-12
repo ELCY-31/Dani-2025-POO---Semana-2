becdd1e71cda1b897dde00456f6c6dd4164b1130
Elcy Ramos <ed.ramoss@uea.edu.ec>
2025-07-12 23:24:50 +0200

    Semana 7

---
+++ SEMANA 7/Constructores y Destructores en Python.py
@@ -0,0 +1,15 @@
+# Implementación de Constructores y Destructores en Python
+
+# class base
+class Libro:
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+        print(f"Libro '{self.titulo}' de {self.autor} inicializado.")
+
+    def __del__(self):
+        print(f"Libro '{self.titulo}' destruido.")
+
+# Uso de los Constructores y de los Destructores
+mi_libro = Libro("Alas de Sangre", "Rebecca Yarros")
+del mi_libro
